817f43ed53367d6e955a80fd8e350c82a58a1e62
Yizhou Sun <yizhou.sun78@gmail.com>
2020-06-21 22:39:58 -0400

    Replace hardcoded robots by fetching profiles online

--- src/App.js
+++ src/App.js
@@ -9,11 +9,16 @@ class App extends Component {
 	constructor () {
 		super();
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''	
 		}
 	}
 
+	componentDidMount() {
+		fetch ('https://jsonplaceholder.typicode.com/users')
+		  .then(response => response.json())
+		  .then(users => this.setState({robots: users}));
+	}
 	onSearchChange = (event) => {
 		this.setState({searchfield: event.target.value})
 	} //on search box change we update the state
@@ -23,13 +28,18 @@ class App extends Component {
 		const filteredRobots = this.state.robots.filter(robots => {
 			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		})
-		return (
-			<div className = 'tc'>	
-				<h1 className = 'f1'>Robots Friends </h1>
-				<SearchBox searchChange = {this.onSearchChange} />
-				<CardList robots = {filteredRobots} />
-			</div>
-		)	
+
+		if (this.state.robots.length === 0) {
+			return <h1>Loading</h1>
+		}else {
+			return (
+				<div className = 'tc'>	
+					<h1 className = 'f1'>Robots Friends </h1>
+					<SearchBox searchChange = {this.onSearchChange} />
+					<CardList robots = {filteredRobots} />
+				</div>
+			)	
+		}
 	}
 }
 
