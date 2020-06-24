2405710d3c70300a81f7b9cca88d2f9453036d80
Yizhou Sun <yizhou.sun78@gmail.com>
2020-06-24 19:00:08 -0400

    Refactor CardList

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,21 +2,23 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({robots}) => {
-	const cardsArray = robots.map((user, index) => {
-			return (
-				<Card 
-					key={index} 
-					id= {robots[index].id} 
-					name= {robots[index].name} 
-					email={robots[index].email}
-				/>
-			)
-	})
-	return (
-		<div>
-			{cardsArray}
+	
+    return (
+    	<div>  
+			{
+				robots.map((user, index) => {
+					return (
+						<Card 
+							key={index} 
+							id= {robots[index].id} 
+							name= {robots[index].name} 
+							email={robots[index].email}
+						/>
+					)
+				})
+			}
 		</div>
-	);
+	)
 }
 
 export default CardList;
